88800106769f5d39ba99814f35de9822a0ebb77e
김정근 <gogoogjg@tripstore.kr>
2022-07-15 18:39:14 +0900

    test

---
+++ src/main/kotlin/com/example/citest/User.kt
@@ -0,0 +1,14 @@
+package com.example.citest
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val Id: Long,
+    val name: String,
+)
