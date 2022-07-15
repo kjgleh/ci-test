7493b1cde44ad4370cd02d9ff365ba8fdaf19506
김정근 <gogoogjg@tripstore.kr>
2022-07-15 14:56:02 +0900

    test

---
+++ src/test/kotlin/com/example/citest/CiTestApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.citest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CiTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
